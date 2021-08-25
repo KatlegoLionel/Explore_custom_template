f3fd97d4de662e9df4396abf3dec73d3f2788a9a
KatlegoLionel <56653169+KatlegoLionel@users.noreply.github.com>
2021-08-25 23:46:21 +0200

    fixing of the cookiecutter hooks

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
+git init
+
 echo "Create environment with information"
 
-conda create -y --name myenv_2 python=3.8.8 pip
-
-source ~/anaconda3/etc/profile.d/conda.sh
-
-echo "conda activate myenv_2"
+conda create -y --name {{cookiecutter.environment_name}} python={{cookiecutter.python_version}}
---
+++ {{cookiecutter.project}}/setup/setup_project.sh
@@ -0,0 +1,12 @@
+ read -r -p "This script is intended for setting up an projects on a new device, do
+ you want to continue?  [y/N]:" response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+  echo "Setting up project"
+  conda create -y --name {{cookiecutter.environment_name}} python={{cookiecutter.python_version}}
+  source ~/anaconda3/etc/profile.d/conda.sh
+  conda activate {{cookiecutter.environment_name}}
+  pip install -r requirements.txt
+else
+  echo "Exiting..."
+fi
